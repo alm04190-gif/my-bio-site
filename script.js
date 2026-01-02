fd84acfd085b39e1fa93fbedc6ab06dd1bb04658
alm04190-gif <alm04190@gmail.com>
2026-01-03 01:54:51 +0600

    Improve theme toggle to change icon on click
    Enhance theme toggle functionality to update icon based on theme.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-// Theme Toggle functionality
-const themeToggle = document.getElementById('themeToggle');
 
-themeToggle.addEventListener('click', () => {
-  document.body.classList.toggle('light');
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click",()=>{
+  document.body.classList.toggle("light");
+  toggle.innerHTML = document.body.classList.contains("light")
+    ? '<i class="fas fa-moon"></i>'
+    : '<i class="fas fa-sun"></i>';
 });
